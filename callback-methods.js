594e8ca4fbd674b07184eb70d73d40ae183fd305
Martin Acosta <tinchoz49@gmail.com>
2020-04-19 11:46:00 -0300

    Renamed destroyData to destroyStorage

--- callback-methods.js
+++ callback-methods.js
@@ -17,6 +17,5 @@ module.exports = [
   'append',
   'flush',
   'audit',
-  'destroy',
   'destroyStorage'
 ]
--- tests/all.js
+++ tests/all.js
@@ -1,3 +1,4 @@
+const test = require('tape')
 const proxyquire = require('proxyquire').noCallThru()
 const ram = require('random-access-memory')
 
@@ -8,11 +9,24 @@ function create (key, opts) {
   return toCallback(hypercorePromise(ram, key, opts))
 }
 
+function hypercore (...args) {
+  return toCallback(hypercorePromise(...args))
+}
+
+const callbackTested = {}
+callbackMethods.forEach(method => {
+  // ignore open since it's an alias of ready
+  if (method === 'open') return
+  callbackTested[method] = false
+})
+
 function toCallback (feed) {
   return new Proxy(feed, {
     get (target, propKey) {
       const value = Reflect.get(target, propKey)
       if (callbackMethods.includes(propKey)) {
+        callbackTested[propKey] = true
+
         return (...args) => {
           if (typeof args[args.length - 1] === 'function') {
             const cb = args.pop()
@@ -59,4 +73,14 @@ const tests = [
 ]
 
 // We convert the promise style into callbacks (again) to test against the original hypercore test code, if the promises are ok, the callbacks should work fine.
-tests.forEach(test => proxyquire(`hypercore/test/${test}`, { './helpers/create': create }))
+tests.forEach(test => proxyquire(`hypercore/test/${test}`, { './helpers/create': create, '../': hypercore }))
+
+test('all callback methods must be tested', async function (t) {
+  t.plan(1)
+
+  const result = Object.keys(callbackTested).reduce((prev, curr) => {
+    return prev && callbackTested[curr]
+  }, true)
+
+  t.ok(result, `callback methods passed ${JSON.stringify(callbackTested)}`)
+})
